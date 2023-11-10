3dd96927cbea5a201ed22972f51f6212bc1e6582
dcen7 <mdcengiz@gmail.com>
2023-11-10 10:07:47 -0600

    Initial Commit

---
+++ BestSellerBooks/BestSellerBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestSellerBooksApp.swift
+//  BestSellerBooks
+//
+//  Created by Deniz Cengiz on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BestSellerBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
