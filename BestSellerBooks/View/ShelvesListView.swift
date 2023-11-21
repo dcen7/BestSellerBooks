d87084dac54b7aa7718da6c20e59548915540fe4
dcen7 <mdcengiz@gmail.com>
2023-11-21 13:25:04 -0600

    extract empty shelve view

--- BestSellerBooks/View/ShelvesListView.swift
+++ BestSellerBooks/View/ShelvesListView.swift
@@ -9,24 +9,12 @@ import SwiftUI
 
 struct ShelvesListView: View {
   let books: [Book]
-  @Environment(\.colorScheme)
-  var colorScheme
 
   var body: some View {
     NavigationStack {
       ZStack {
         if books.isEmpty {
-          VStack(spacing: 20) {
-            CustomTextView(text: "There are no books in the shelve...", size: 17)
-              .padding(.top)
-            CustomTextView(text: "You can add from book detail screen.", size: 12)
-              .padding()
-          }
-          .padding()
-          .frame(maxWidth: 300)
-          .background(colorScheme == .dark ? .blue : .white)
-          .cornerRadius(21)
-          .shadow(radius: 10, x: 5, y: 5)
+          EmptyShelveView()
         } else {
           ScrollView {
             VStack {
