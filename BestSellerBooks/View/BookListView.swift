605b791f42ca96cc288542d31b81edd3d718cbc1
dcen7 <mdcengiz@gmail.com>
2023-11-21 12:00:28 -0600

    add communication screens for emypy lists

--- BestSellerBooks/View/BookListView.swift
+++ BestSellerBooks/View/BookListView.swift
@@ -11,46 +11,61 @@ struct BookListView: View {
   @ObservedObject var store: BookStore
   @Binding var fetchObjectsTask: Task<Void, Error>?
   @MainActor @State private var showDownloadFailedAlert = false
+  @State private var showNoConnectionView = false
 
   var body: some View {
     NavigationStack {
-      List(store.books, id: \.self) { book in
-        NavigationLink(value: book) {
-          HStack {
-            ImageView(image: book.bookImage)
-              .padding(.trailing)
-            VStack(alignment: .leading) {
-              CustomTextView(text: "\(book.author)", size: 12)
-                .bold()
-                .padding(.top)
-              Spacer()
-              Text("\(book.title)")
-                .font(.system(size: 14, weight: .bold, design: .serif))
-              Spacer()
-              CustomTextView(text: "\(book.description)", size: 13)
-                .lineLimit(3)
-                .padding(.bottom)
+      ZStack {
+        if showNoConnectionView {
+          VStack(spacing: 20) {
+            CustomTextView(text: "There was a network issue...", size: 17)
+            CustomTextView(text: "Please try again later", size: 12)
+          }
+          .padding()
+          .frame(maxWidth: 300)
+          .background()
+          .cornerRadius(21)
+          .shadow(radius: 10, x: 5, y: 5)
+        }
+        List(store.books, id: \.self) { book in
+          NavigationLink(value: book) {
+            HStack {
+              ImageView(image: book.bookImage)
+                .padding(.trailing)
+              VStack(alignment: .leading) {
+                CustomTextView(text: "\(book.author)", size: 12)
+                  .bold()
+                  .padding(.top)
+                Spacer()
+                Text("\(book.title)")
+                  .font(.system(size: 14, weight: .bold, design: .serif))
+                Spacer()
+                CustomTextView(text: "\(book.description)", size: 13)
+                  .lineLimit(3)
+                  .padding(.bottom)
+              }
             }
           }
         }
-      }
-      .listStyle(.plain)
-      .navigationDestination(for: Book.self) { book in
-        // swiftlint:disable:next force_unwrapping
-        BookDetailView(store: store, book: $store.books.first { $0.id == book.id }!)
-      }
-      .navigationTitle("NYTimes Best Sellers")
-      .task {
-        if store.books.isEmpty {
-          downloadBooks()
+        .listStyle(.plain)
+        .navigationDestination(for: Book.self) { book in
+          // swiftlint:disable:next force_unwrapping
+          BookDetailView(store: store, book: $store.books.first { $0.id == book.id }!)
         }
-      }
-      .alert("Download Failed", isPresented: $showDownloadFailedAlert) {
-        Button("Try Again") {
-          downloadBooks()
+        .navigationTitle("NYTimes Best Sellers")
+        .task {
+          if store.books.isEmpty {
+            downloadBooks()
+          }
         }
-        Button("Dismiss") {
-          showDownloadFailedAlert = false
+        .alert("Download Failed", isPresented: $showDownloadFailedAlert) {
+          Button("Try Again") {
+            downloadBooks()
+          }
+          Button("Dismiss") {
+            showDownloadFailedAlert = false
+            showNoConnectionView = true
+          }
         }
       }
     }
--- BestSellerBooks/View/ShelvesListView.swift
+++ BestSellerBooks/View/ShelvesListView.swift
@@ -9,27 +9,45 @@ import SwiftUI
 
 struct ShelvesListView: View {
   let books: [Book]
+  @Environment(\.colorScheme)
+  var colorScheme
 
   var body: some View {
     NavigationStack {
-      ScrollView {
-        VStack {
-          ForEach(books, id: \.self) { book in
-            HStack {
-              ImageView(image: book.bookImage)
-                .padding(.trailing)
-              VStack(alignment: .leading, spacing: 5) {
-                Text("\(book.author)")
-                  .font(.system(size: 12, weight: .ultraLight, design: .serif))
-                Text("\(book.title)")
-                  .font(.system(size: 14, weight: .bold, design: .serif))
+      ZStack {
+        if books.isEmpty {
+          VStack(spacing: 20) {
+            CustomTextView(text: "There are no books in the shelve...", size: 17)
+              .padding(.top)
+            CustomTextView(text: "You can add from book detail screen.", size: 12)
+              .padding()
+          }
+          .padding()
+          .frame(maxWidth: 300)
+          .background(colorScheme == .dark ? .blue : .white)
+          .cornerRadius(21)
+          .shadow(radius: 10, x: 5, y: 5)
+        } else {
+          ScrollView {
+            VStack {
+              ForEach(books, id: \.self) { book in
+                HStack {
+                  ImageView(image: book.bookImage)
+                    .padding(.trailing)
+                  VStack(alignment: .leading, spacing: 5) {
+                    Text("\(book.author)")
+                      .font(.system(size: 12, weight: .ultraLight, design: .serif))
+                    Text("\(book.title)")
+                      .font(.system(size: 14, weight: .bold, design: .serif))
+                  }
+                  Spacer()
+                  // swiftlint:disable:next force_unwrapping
+                  Link("Buy", destination: URL(string: "\(book.amazonProductURL)")!)
+                    .modifier(ButtonViewModifier())
+                }
+                .padding()
               }
-              Spacer()
-              // swiftlint:disable:next force_unwrapping
-              Link("Buy", destination: URL(string: "\(book.amazonProductURL)")!)
-                .modifier(ButtonViewModifier())
             }
-            .padding()
           }
         }
       }
